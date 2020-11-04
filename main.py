cba67388c51921bcbddf0077b70c8dbbd0e812be
mfnalex <1122571+mfnalex@users.noreply.github.com>
2020-11-04 02:40:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
 import time
 import i2clcd
 import locale
@@ -38,6 +38,12 @@ def debug(text):
     if debugEnabled:
         print("debug: "+text)
         
+def drawLine(char):
+    result = ""
+    for i in range(0,DISPLAY_WIDTH):
+        result = result + char
+    return result
+        
 def getButton():
     result = 0
     if GPIO.input(PIN_LEFT) == GPIO.HIGH:
@@ -48,7 +54,8 @@ def getButton():
         result = result + BUTTON_RESET
     return result
     
-def showText(text, start):
+def showText(text, start = 0):
+    text = remove_non_ascii(text)
     textWrapped = textwrap.wrap(text,DISPLAY_WIDTH)
     if start < 0:
         debug("start < 0")
@@ -104,7 +111,7 @@ def mainMenu(start):
     elif button == BUTTON_RIGHT and start < len(menu)-2:
         mainMenu(start+1)
     elif button == BUTTON_RESET:
-        print("Main Menu: "+menu[start])
+        debug("Main Menu: "+menu[start])
         startRoutine(menu[start],start)
     else:
         mainMenu(start)
@@ -125,7 +132,7 @@ def remove_non_ascii(text):
     text = text.replace("ö","oe")
     text = text.replace("Ü","Ue")
     text = text.replace("Ö","Oe")
-    text = text.replace("A","Ae")
+    text = text.replace("Ä","Ae")
     text = text.replace("ß","ss")
     return unidecode(text)
     
@@ -139,33 +146,35 @@ def routine_wikipedia():
     os.system("curl -Ls -o /dev/null -w %{url_effective} https://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite > /tmp/wikipedialink")
     articleName = open("/tmp/wikipedialink").read()
     articleName = urllib_unquote(remove_prefix(articleName, "https://de.wikipedia.org/wiki/"))
-    print("Random article: " + articleName)
+    debug("Random article: " + articleName)
     lcd.print_line("Artikel",0,"CENTER")
     lcd.print_line("laden ...",1,"CENTER")
-    downloadCommand = "echo 'Artikel: "+articleName.replace("_"," ")+" ================' > /tmp/wikipedia; wikit "+articleName+" -l de >> /tmp/wikipedia"
-    print("DownloadCommand:")
-    print(downloadCommand)
+    downloadCommand = "echo 'Artikel: "+articleName.replace("_"," ")+" "+drawLine("=")+"' > /tmp/wikipedia; wikit "+articleName+" -l de >> /tmp/wikipedia"
+    debug("DownloadCommand:")
+    debug(downloadCommand)
     os.system(downloadCommand)
     lcd.print_line("Artikel",0,"CENTER")
     lcd.print_line("verarbeiten ...",1,"CENTER")
     file = open("/tmp/wikipedia")
-    article = textwrap.wrap(remove_non_ascii(file.read()),width=DISPLAY_WIDTH)
-    print(article)
+    #article = textwrap.wrap(remove_non_ascii(file.read()),width=DISPLAY_WIDTH)
+    article = file.read()
+    debug(article)
     lcd.print_line("Fertig!",0)
-    show_wikipedia(article, 0)
+    showText(article)
+    #show_wikipedia(article, 0)
     
-def show_wikipedia(article, start):
-    if start < len(article):
-        lcd.print_line(article[start],0)
-    if start+1 < len(article):
-        lcd.print_line(article[start+1],1)
-    button = waitGetButton()
-    if button == BUTTON_LEFT and start > 1:
-        show_wikipedia(article,start-2)
-    elif button == BUTTON_RIGHT and start < len(article)-3:
-        show_wikipedia(article,start+2)
-    elif button != BUTTON_RESET:
-        print("quit Wikipedia")
+#def show_wikipedia(article, start):
+#    if start < len(article):
+#        lcd.print_line(article[start],0)
+#    if start+1 < len(article):
+#        lcd.print_line(article[start+1],1)
+#    button = waitGetButton()
+#    if button == BUTTON_LEFT and start > 1:
+#        show_wikipedia(article,start-2)
+#    elif button == BUTTON_RIGHT and start < len(article)-3:
+#        show_wikipedia(article,start+2)
+#    elif button != BUTTON_RESET:
+#        print("quit Wikipedia")
     
 def routine_uhr():
     while waitForButton(BUTTON_RESET,True):
@@ -189,7 +198,7 @@ def menu_speedtest(start,download,upload,ping):
     elif button == BUTTON_RIGHT and start < len(menu)-2:
         menu_speedtest(start+1, download, upload, ping)
     elif button == BUTTON_RESET:
-        print("Speedtest Menu: "+menu[start])
+        debug("Speedtest Menu: "+menu[start])
         startSpeedtestRoutine(menu[start],start, download, upload, ping)
     else:
         menu_speedtest(start,download,upload,ping)
@@ -213,7 +222,7 @@ def startSpeedtestRoutine(routine, start, download, upload, ping):
         menu_speedtest(start,download,upload,ping)
         
 def routine_speedtest():
-    print("Speedtest... this can take a while")
+    debug("Speedtest... this can take a while")
     lcd.print_line("Teste Haehnchens",0,"CENTER");
     lcd.print_line("Internet ...",1,"CENTER")
     st = speedtest.Speedtest()
@@ -226,25 +235,25 @@ def routine_speedtest():
     best_server = st.get_best_server()
     ping = "? ms"
     for key, value in best_server.items():
-        print(key, ' : ', value)
+        debug(key, ' : ', value)
         if key == "latency":
             ping = f'{value:.3f}'  + " ms"
-    print("MBit/s Download: "+mbit_download_string)
-    print("MBit/s Upload: "+mbit_upload_string)
+    debug("MBit/s Download: "+mbit_download_string)
+    debug("MBit/s Upload: "+mbit_upload_string)
     menu_speedtest(0,mbit_download_string,mbit_upload_string,ping)
     #while waitForButton(BUTTON_RESET,True):
     #    time.sleep(0.01)
         
         
-showText("eine zeile zweite zeile dritte zeile vierte zeile fuenfte zeile",0)
+#showText("eine zeile zweite zeile dritte zeile vierte zeile fuenfte zeile",0)
 showText("Herzlich willkommen zu Haehnchens Geraet, liebste Hydraehnchen! Mit links und rechts kannst du nach oben und unten scrollen und mit dem einsamen Knopf bestaetigen. 1234567",0)
     
-print("Splash Start")
+debug("Splash Start")
 lcd.print_line("Hydraehnchen's",0,"CENTER")
 lcd.print_line("Geraet",1,"CENTER")
 time.sleep(WAIT_SPLASH)
 lcd.clear()
-print("Splash End")
+debug("Splash End")
 
 
 mainMenu(0)
